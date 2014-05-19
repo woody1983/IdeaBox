4ecd9abcfe10b332aede7e302a7e9c11f1aded66
Johnny <unix1983@gmail.com>
2014-05-19 10:14:57 +0000

    Automatic Reloading

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'sinatra', require: 'sinatra/base'
+gem 'sinatra-contrib', require: 'sinatra/reloader'
+
 
--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@ require 'bundler'
 Bundler.require
 
 class IdeaBoxApp < Sinatra::Base
+
+  configure :development do
+    register Sinatra::Reloader
+  end #For Automatic Reloading
+
   get '/' do
     erb :index
   end

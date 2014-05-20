550160e6eba626d5fc3d61a3e9eb8175e1e2bff0
Johnny <unix1983@gmail.com>
2014-05-20 03:06:57 +0000

    Add Post and save ideas

--- app.rb
+++ app.rb
@@ -7,9 +7,21 @@ class IdeaBoxApp < Sinatra::Base
     register Sinatra::Reloader
   end #For Automatic Reloading
 
-  get '/' do
-    erb :index
+  not_found do
+    erb :error
   end
+
+
+  get '/' do
+    erb :index,locals: {ideas: Idea.all}
+  end
+
+  post '/' do
+    idea = Idea.new(params['idea_title'],params['idea_description'])
+    idea.save
+    redirect '/'
+  end
+
 
 end
 
---
+++ idea.rb
@@ -0,0 +1,38 @@
+require 'yaml/store'
+class Idea
+  attr_reader :title, :description
+
+  def initialize(title, description)
+    @title = title
+    @description = description
+  end
+
+  def self.all
+    raw_ideas.map do |data|
+      new(data[:title], data[:description])
+    end
+  end
+
+  def self.raw__ideas
+    database.transaction do |db|
+      db['ideas'] || []
+    end
+  end
+
+  def save
+    database.transaction do |db|
+      db['ideas'] ||= []
+      db['ideas'] << {title: @title, description: @description}
+    end  
+  end
+
+  def self.database
+    @database ||= YAML::Store.new "ideabox"
+  end
+  def database
+    Idea.database
+  end
+
+
+end
+
